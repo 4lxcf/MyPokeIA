b79fe4c0f6e8e2d126ea8860628d3f57bd841f4f
Alexandre <alexandrecferreira11@gmail.com>
2024-09-22 18:33:54 -0300

    perf: Novas_Funções_&_Organização_PokemonRedEnv | Update_Modo_de_Salvamento

--- PPO/PokemonRedEnv.py
+++ PPO/PokemonRedEnv.py
@@ -5,19 +5,60 @@ from pyboy import PyBoy
 
 N_DISCRETE_ACTIONS = 5 # Cima, baixo, direita, esquerda, a
 N_CHANNELS = 3
+MAX_STEPS = 2048
 
 class PokemonRedEnv(gym.Env):
     # metadata = {"render_modes": ["human"], "render_fps": 30}
     
     def __init__(self, rom_path):
+        self.pyboy = PyBoy(rom_path)
+        self.pyboy.set_emulation_speed(6)
+
         self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
         self.observation_space = spaces.Box(low=np.array([0, 0, 0]), high=np.array([255, 255, 255]), dtype=np.uint8)
 
-        self.pyboy = PyBoy(rom_path)
-        self.pyboy.set_emulation_speed(0)
+    def reset(self, seed=None, options={}):
+        # Se o seed for fornecido, defina a semente do ambiente
+        if seed is not None:
+            np.random.seed(seed)
+
+        # Carregar o estado salvo no início do jogo
+        with open('D:\Dev\MyPokeIA\PokemonRed.gb.state', "rb") as f:
+            self.pyboy.load_state(f)
+
+        self.agent_stats = []
+        self.visited_positions = {}
+        self.step_count = 0
+        self.max_steps = MAX_STEPS
+        self.max_levels = 0
+        self.max_visited_positions = 0
+
+        # Ler os valores dos três endereços de memória
+        map_id = self.pyboy.memory[0xD35E]  # ID do mapa
+        pos_x = self.pyboy.memory[0xD362]   # Posição X do personagem
+        pos_y = self.pyboy.memory[0xD361]   # Posição Y do personagem
+        observation = np.array([map_id, pos_x, pos_y], dtype=np.uint8)
+
+        return observation, {}
 
     def step(self, action):
-        # Executa a ação usando a função button()
+        self.make_action(action)
+        self.update_agent_stats()
+
+        map_id, pos_x, pos_y = (self.pyboy.memory[0xD35E], self.pyboy.memory[0xD362], self.pyboy.memory[0xD361])
+        self.step_count += 1
+
+        actual_position = (map_id, pos_x, pos_y)
+        if actual_position not in self.visited_positions:
+            self.visited_positions[actual_position] = self.step_count
+
+        observation = np.array([map_id, pos_x, pos_y], dtype=np.uint8) 
+        reward = self.calculate_reward() 
+        truncated = self.step_limit_count()
+        info = {f"Agent_Stats:": self.agent_stats} # Infos adicionais
+        return observation, reward, False, truncated, info
+
+    def make_action(self, action):
         if action == 0:
                 self.pyboy.button_press("up")
         elif action == 1:
@@ -29,124 +70,45 @@ class PokemonRedEnv(gym.Env):
         elif action == 4:
                 self.pyboy.button_press("a")
 
-        self.pyboy.tick() # Tick do emulador
+        self.pyboy.tick(8) # Tick do emulador
         self.pyboy.button_release("up")
         self.pyboy.button_release("down")
         self.pyboy.button_release("right")
         self.pyboy.button_release("left")
         self.pyboy.button_release("a")
-
-        # Obter nova observação
-        map_id = self.pyboy.memory[0xD35E]
-        pos_x = self.pyboy.memory[0xD362]
-        pos_y = self.pyboy.memory[0xD361]
-        observation = np.array([map_id, pos_x, pos_y], dtype=np.uint8)
-
-        # Verificar se o personagem está em batalha
-        self.in_battle = self.pyboy.memory[0xD057] == 1
-
-        # Verificar se o episódio terminou
-        terminated = False
-        truncated = False
-
-        actual_position = (map_id, pos_x, pos_y)
-
-        # Calcular a recompensa de posição
-        reward = self.calculate_reward((map_id, pos_x, pos_y))
-
-        if not self.in_battle:              
-            # Atualizar a lista de posições visitadas
-            if actual_position in self.visited_positions:
-                self.visited_positions[actual_position] += 1
-            else:
-                self.visited_positions[actual_position] = 1
-
-        # Verificar se alguma posição foi visitada e resetar o ambiente caso tenha sido muitas vezes
-        if self.visited_positions[actual_position] >= 150:
-            print(f"Resetting environment. Position {actual_position} visited {self.visited_positions[actual_position]} times.")
-            reward = -10
-            observation, terminated = self.reset()
-            return observation, reward, True, False, {}  # Retornar com `terminated = True`
-
-        # Verifica se os níveis dos Pokémon aumentaram e adiciona à recompensa
-        current_levels_sum = self.get_levels_sum()
-        if current_levels_sum > self.last_levels_sum:
-            reward += (current_levels_sum - self.last_levels_sum)  # Recompensa adicional por aumento de nível
-            self.last_levels_sum = current_levels_sum  # Atualiza o nível total da party
-
-        # Informações adicionais
-        info = {"visited_positions_size": len(self.visited_positions)}
-
-        return observation, reward, terminated, truncated, info
-
-    def reset(self, seed=None, options={}):
-        # Se o seed for fornecido, defina a semente do ambiente
-        if seed is not None:
-            np.random.seed(seed)
-
-        # Carregar o estado salvo no início do jogo
-        with open('D:\Dev\MyPokeIA\PokemonRed.gb.state', "rb") as f:
-            self.pyboy.load_state(f)
-
-        # Ler os valores dos três endereços de memória
-        map_id = self.pyboy.memory[0xD35E]  # ID do mapa
-        pos_x = self.pyboy.memory[0xD362]   # Posição X do personagem
-        pos_y = self.pyboy.memory[0xD361]   # Posição Y do personagem
-        observation = np.array([map_id, pos_x, pos_y], dtype=np.uint8)
-
-        # Inicializar o conjunto de posições visitadas
-        self.visited_positions = {}
-        actual_position = (map_id, pos_x, pos_y)
-        self.visited_positions[actual_position] = 1
-
-        # Inicializar áreas e posições exploradas
-        self.explored_areas = set()
-        self.explored_positions = set()
-        
-        # Armazena o nível total inicial da party
-        self.last_levels_sum = self.get_levels_sum()
-
-        # Inicializar terminated como False
-        terminated = False
-
-        return observation, terminated
     
-    def calculate_reward(self, observation):
-    # Penalize positions revisited multiple times
-        # if not self.in_battle:
-        #     if observation in self.visited_positions:
-        #         if self.visited_positions[observation] > 50:  # Definir um limite de revisitações aceitáveis
-        #             reward = -0.5  # Penalização maior para revisitações excessivas
-        #         else:
-        #             reward = -0.1  # Penalização leve para revisitações
-        #     else:
-        #         reward = 1  # Recompensa por descobrir uma nova posição
-        # else:
-        #      reward = 0
-        # return reward
+    def update_agent_stats(self):
+         map_id, pos_x, pos_y = (self.pyboy.memory[0xD35E], self.pyboy.memory[0xD362], self.pyboy.memory[0xD361])
+         levels = [self.pyboy.memory[e] for e in [0xD18C, 0xD1B8, 0xD1E4, 0xD210, 0xD23C, 0xD268]]
+         self.agent_stats.append(
+              {
+                   "step": self.step_count,
+                   "pos_x": pos_x,
+                   "pos_y": pos_y,
+                   "map": map_id,
+                   "visited_positions_count": len(self.visited_positions),
+                   "levels": levels,
+                   "levels_sum": sum(levels)
+              }
+         )
 
-        map_id, pos_x, pos_y = observation
+    def step_limit_count(self):
+        step_limit_flag = self.step_count >= self.max_steps
+        return step_limit_flag
+
+    def calculate_reward(self):
         reward = 0
+        level_sum = self.get_levels_sum()
+        visited_positions_count = len(self.visited_positions)
 
-        if not self.in_battle:
-        # Verifica se o agente está em uma nova área
-            if map_id not in self.explored_areas:
-                reward += 10  # Recompensa por descobrir uma nova área
-                self.explored_areas.add(map_id)
+        if level_sum > self.max_levels:
+             reward += level_sum - self.max_levels
+             self.max_levels = level_sum
 
-        # Verifica se o agente está em uma nova posição dentro da área
-            position_key = (map_id, pos_x, pos_y)
-            if position_key not in self.explored_positions:
-                reward += 2  # Recompensa por explorar uma nova posição dentro da área
-                self.explored_positions.add(position_key)
-            else:
-                reward -= 0.1  # Penalização leve por revisitar uma posição
-
-        # Penaliza revisitações excessivas (mais de 50 vezes na mesma posição)
-            if self.visited_positions.get(position_key, 0) > 50:
-                reward -= 1  # Penalização maior para revisitações excessivas
-        # print(f"visited_positions:{self.visited_positions} | explored_areas:{self.explored_areas}")
-
+        if visited_positions_count > self.max_visited_positions:
+             reward += 0.04 * (visited_positions_count - self.max_visited_positions)
+             self.max_visited_positions = visited_positions_count
+        
         return reward
     
     def get_levels_sum(self):
--- PPO/poke_learn.py
+++ PPO/poke_learn.py
@@ -1,11 +1,10 @@
 from stable_baselines3 import PPO
 import os
-import time
 from PokemonRedEnv import PokemonRedEnv
 
 # Paths para salvar
-models_dir = f"models/{int(time.time())}"
-logdir = f"logs/{int(time.time())}"
+models_dir = f"models/"
+logdir = f"logs/"
 
 # Criação das pastas, caso ainda não existam
 if not os.path.exists(models_dir):
@@ -19,9 +18,10 @@ env.reset()
 
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir)
 
-TIMESTEPS = 50000
-for i in range(1000):
-    model.learn(total_timesteps=TIMESTEPS, progress_bar=True, reset_num_timesteps=False, tb_log_name="PPO")
-    model.save(f"{models_dir}/{TIMESTEPS*i}")
+episodes = 50
+TIMESTEPS = 2048*50
+for episode in episodes:
+    model.learn(total_timesteps=TIMESTEPS, progress_bar=True, tb_log_name=f"PPO-ep{episode}")
+    model.save(f"{models_dir}/model_TIMESTEPS{TIMESTEPS}_LR{model.learning_rate}_EP{episode}")
 
 env.close()
